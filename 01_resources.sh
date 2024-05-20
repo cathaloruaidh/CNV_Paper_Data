073b429763986e0a7b259e9b759a2a3edc8e6098
Cathal Ormond <32680817+cathaloruaidh@users.noreply.github.com>
2024-05-20 15:41:00 +0100

    Benchmarking on GRCh38 instead

--- 01_resources.sh
+++ 01_resources.sh
@@ -13,9 +13,8 @@
 mkdir output
 
 
-# get the GRCh38 to GRCh37 chain file
-wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz -P resource 
-
+# get the GRCh37 to GRCh38 chain file
+wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz -P resource
 
 
 
@@ -30,6 +29,41 @@ bcftools query -i 'FILTER="PASS" && SVTYPE="DEL" && (REPTYPE="SIMPLEDEL" || REPT
 	| awk -v OFS="\t" '{ print $1, $2, $2 - $3, "HG002_Tier1_DEL_" NR} ' \
 	| bedtools sort -i - \
 	> resource/HG002_SVs_Tier1_v0.6.DEL.bed
+
+
+
+# extract benchmark duplications
+bcftools query -i 'FILTER="PASS" && SVTYPE="INS" && REPTYPE="DUP" && TRall="FALSE"' -f 'chr%CHROM\t%POS\t%SVLEN\n' resource/HG002_SVs_Tier1_v0.6.vcf.gz \
+	| awk -v OFS="\t" '{ print $1, $2, $2 - $3, "HG002_Tier1_DUP_" NR} ' \
+	| bedtools sort -i - \
+	> resource/HG002_SVs_Tier1_v0.6.DUP.bed
+
+
+
+# lift the GS calls to GRCh38
+liftOver resource/HG002_SVs_Tier1_v0.6.DEL.bed resource/hg19ToHg38.over.chain.gz resource/HG002_SVs_Tier1_v0.6.DEL.lift_GRCh38.bed resource/HG002_SVs_Tier1_v0.6.DEL.reject_GRCh38.bed
+
+
+liftOver resource/HG002_SVs_Tier1_v0.6.DUP.bed resource/hg19ToHg38.over.chain.gz resource/HG002_SVs_Tier1_v0.6.DUP.lift_GRCh38.bed resource/HG002_SVs_Tier1_v0.6.DUP.reject_GRCh38.bed
+
+
+
+# split GS files by size for benchmarking
+awk '$3-$2 > 1000' resource/2023.5.11_NA12878_GS_STRICT_super_clean_DELs.uniq_no_Mills_chrX.lift_GRCh38.bed > resource/2023.5.11_NA12878_GS_STRICT_super_clean_DELs.uniq_no_Mills_chrX.lift_GRCh38.gt1kbp.bed
+
+awk '$3-$2 <= 1000' resource/2023.5.11_NA12878_GS_STRICT_super_clean_DELs.uniq_no_Mills_chrX.lift_GRCh38.bed > resource/2023.5.11_NA12878_GS_STRICT_super_clean_DELs.uniq_no_Mills_chrX.lift_GRCh38.le1kbp.bed
+
+
+awk '$3-$2 > 1000' resource/HG002_SVs_Tier1_v0.6.DEL.lift_GRCh38.bed > resource/HG002_SVs_Tier1_v0.6.DEL.lift_GRCh38.gt1kbp.bed
+
+awk '$3-$2 <= 1000' resource/HG002_SVs_Tier1_v0.6.DEL.lift_GRCh38.bed > resource/HG002_SVs_Tier1_v0.6.DEL.lift_GRCh38.le1kbp.bed
+
+
+awk '$3-$2 > 1000' resource/HG002_SVs_Tier1_v0.6.DUP.lift_GRCh38.bed > resource/HG002_SVs_Tier1_v0.6.DUP.lift_GRCh38.gt1kbp.bed
+
+awk '$3-$2 <= 1000' resource/HG002_SVs_Tier1_v0.6.DUP.lift_GRCh38.bed > resource/HG002_SVs_Tier1_v0.6.DUP.lift_GRCh38.le1kbp.bed
+
+
 
 
 echo -e "\n\nDone! \n\n"
--- 02_benchmark_Figure3.sh
+++ 02_benchmark_Figure3.sh
@@ -6,22 +6,23 @@
 # 
 # Benchmark the CNV calls for the two evaluation
 # samples against the curated gold standard
-# deletion calls. This calculates the recall and 
-# precision of the call sets. 
+# CNV calls. This calculates the recall and 
+# precision of the query call sets. 
 ##################################################
 
 
 
 # header line
-echo -e "Sample\tPipeline\tRecall\tPrecision" > output/Benchmarking_results.txt
+echo -e "Sample\tPipeline\tRecall\tPrecision" > output/Benchmarking_results.DEL.txt
+echo -e "Sample\tPipeline\tRecall\tPrecision" > output/Benchmarking_results.DUP.txt
 
 
 
-#  NA12878
-for FILE in data/NA12878.*.DEL.lift_hg19.bed
+#  NA12878 DEL
+for FILE in data/NA12878.*.DEL.bed
 do 
-	GS="resource/2023.5.11_NA12878_GS_STRICT_super_clean_DELs.uniq_no_Mills_chrX.bed"
-
+	# all CNVs
+	GS="resource/2023.5.11_NA12878_GS_STRICT_super_clean_DELs.uniq_no_Mills_chrX.lift_GRCh38.bed"
 	PL=$( echo ${FILE} | cut -f2 -d'.' )
 
 	COUNT=$( bedtools intersect -a ${GS} -b ${FILE} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
@@ -30,28 +31,87 @@ do
 	COUNT=$( bedtools intersect -a ${FILE} -b ${GS} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
 	PREC=$( echo "scale=6;${COUNT}/$( wc -l < ${FILE} )" | bc )
 
-	echo -e "NA12878\t${PL}\t${RECALL}\t${PREC}"
-done >> output/Benchmarking_results.txt
+	echo -e "NA12878\t${PL}\tAll\t${RECALL}\t${PREC}"
+
+
+
+	# CNVs of length <=1kbp
+	GS="resource/2023.5.11_NA12878_GS_STRICT_super_clean_DELs.uniq_no_Mills_chrX.lift_GRCh38.le1kbp.bed"
+
+	COUNT=$( bedtools intersect -a ${GS} -b ${FILE%bed}le1kbp.bed -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+	RECALL=$( echo "scale=6;${COUNT}/$( wc -l < ${GS} )" | bc )
+
+	COUNT=$( bedtools intersect -a ${FILE%bed}le1kbp.bed -b ${GS} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+	PREC=$( echo "scale=6;${COUNT}/$( wc -l < ${FILE} )" | bc )
+
+	echo -e "NA12878\t${PL}\t<=1kbp\t${RECALL}\t${PREC}"
+
+
+
+	# CNVs of length >1kbp
+	GS="resource/2023.5.11_NA12878_GS_STRICT_super_clean_DELs.uniq_no_Mills_chrX.lift_GRCh38.gt1kbp.bed"
+
+	COUNT=$( bedtools intersect -a ${GS} -b ${FILE%bed}gt1kbp.bed -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+	RECALL=$( echo "scale=6;${COUNT}/$( wc -l < ${GS} )" | bc )
+
+	COUNT=$( bedtools intersect -a ${FILE%bed}gt1kbp.bed -b ${GS} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+	PREC=$( echo "scale=6;${COUNT}/$( wc -l < ${FILE} )" | bc )
+
+	echo -e "NA12878\t${PL}\t>1kbp\t${RECALL}\t${PREC}"
+done >> output/Benchmarking_results.DEL.txt
 
 
 
 
 
 #  HG002
-for FILE in data/HG002.*.DEL.lift_hg19.bed
-do 
-	GS="resource/HG002_SVs_Tier1_v0.6.DEL.bed"
-
-	PL=$( echo ${FILE} | cut -f2 -d'.' )
-
-	COUNT=$( bedtools intersect -a ${GS} -b ${FILE} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
-	RECALL=$( echo "scale=6;${COUNT}/$( wc -l < ${GS} )" | bc )
+for CNV in DUP DEL
+do
+	for FILE in data/HG002.*.${CNV}.bed
+	do 
+		GS="resource/HG002_SVs_Tier1_v0.6.${CNV}.lift_GRCh38.bed"
 	
-	COUNT=$( bedtools intersect -a ${FILE} -b ${GS} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
-	PREC=$( echo "scale=6;${COUNT}/$( wc -l < ${FILE} )" | bc )
+		PL=$( echo ${FILE} | cut -f2 -d'.' )
+	
+		COUNT=$( bedtools intersect -a ${GS} -b ${FILE} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+		RECALL=$( echo "scale=6;${COUNT}/$( wc -l < ${GS} )" | bc )
+	
+		COUNT=$( bedtools intersect -a ${FILE} -b ${GS} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+		PREC=$( echo "scale=6;${COUNT}/$( wc -l < ${FILE} )" | bc )
 
-	echo -e "HG002\t${PL}\t${RECALL}\t${PREC}"
-done >> output/Benchmarking_results.txt
+		echo -e "HG002\t${PL}\tAll\t${RECALL}\t${PREC}"
+
+
+
+		# CNVs of length <=1kbp
+		GS="resource/HG002_SVs_Tier1_v0.6.${CNV}.lift_GRCh38.le1kbp.bed"
+
+		COUNT=$( bedtools intersect -a ${GS} -b ${FILE%bed}le1kbp.bed -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+		RECALL=$( echo "scale=6;${COUNT}/$( wc -l < ${GS} )" | bc )
+
+		COUNT=$( bedtools intersect -a ${FILE%bed}le1kbp.bed -b ${GS} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+		PREC=$( echo "scale=6;${COUNT}/$( wc -l < ${FILE} )" | bc )
+
+		echo -e "HG002\t${PL}\t<=1kbp\t${RECALL}\t${PREC}"
+
+
+
+		# CNVs of length >1kbp
+		GS="resource/HG002_SVs_Tier1_v0.6.${CNV}.lift_GRCh38.gt1kbp.bed"
+
+		COUNT=$( bedtools intersect -a ${GS} -b ${FILE%bed}gt1kbp.bed -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+		RECALL=$( echo "scale=6;${COUNT}/$( wc -l < ${GS} )" | bc )
+
+		COUNT=$( bedtools intersect -a ${FILE%bed}gt1kbp.bed -b ${GS} -f 0.5 -r -wa | bedtools sort -i - | uniq | wc -l )
+		PREC=$( echo "scale=6;${COUNT}/$( wc -l < ${FILE} )" | bc )
+
+		echo -e "HG002\t${PL}\t>1kbp\t${RECALL}\t${PREC}"
+
+	done >> output/Benchmarking_results.${CNV}.txt
+done
+
+
+
 
 
 
